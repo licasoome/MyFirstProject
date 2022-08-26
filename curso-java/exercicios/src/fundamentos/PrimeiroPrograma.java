39503bf423ab0dd13c138706d9fa50ade63e6b3c
licasoome <110794199+licasoome@users.noreply.github.com>
2022-08-26 20:16:55 -0300

    Add files via upload

---
+++ curso-java/exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		System.out.println("Hoje tem gol do Ribamar!");
+		System.out.println("Hoje tem gol do Ribamar!");
+		System.out.println("Hoje tem gol do Ribamar!");
+		
+	}
+}
